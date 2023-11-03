0804c26a95697b77637b535844f20a253534b379
abul <abulhasan7bd.gmail.com>
2023-11-03 21:42:01 +0600

    klsd

--- server.js
+++ server.js
@@ -3,36 +3,12 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 app.get("/", (req, res) => {
-  res.end("Home how are yo tuei ekieeepage");
+  res.end(
+    "Home how are y        ddddo tuei xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxekieeepage",
+  );
 
-
-
-
-
-
-
-
-
-
-  
-  res.send("<h1>i love yu</h1>")
+  res.send("<h1>i love yu</h1>");
 });
 
 app.listen(port, () => {
